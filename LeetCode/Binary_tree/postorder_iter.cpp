b57af73e01a06f3c569e768a23a2c4e05ce91b8c
Prateek Arora <prateekarorav2@gmail.com>
2020-03-03 14:39:30 -0500

    completed post order using iterative method

---
+++ LeetCode/Binary_tree/postorder_iter.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL){};
+};
+
+class Solution {
+public:
+    std::vector<int> postorderTraversal(TreeNode* root) {
+        std::vector<int> ans;
+        std::stack<TreeNode*> stk;
+        // TreeNode* curr = root;
+        stk.push(root);
+        while(!stk.empty() )
+        {
+            root = stk.top();
+            ans.push_back(root->val);
+            print(root->val," inside");
+            stk.pop();
+            if(root->left)
+            {
+                stk.push(root->left);
+            }
+            if(root->right)
+            {
+                stk.push(root->right);
+            }
+        }
+        std::reverse(ans.begin(),ans.end());
+        for(auto i:ans)
+        {
+            print(i,"ans");
+        }
+        return ans;
+    }
+    template<typename T, typename T2>
+    void print(T val, T2 name)
+    {
+        std::cout<< "output in " <<name<<" = "<< val << std::endl;
+    }
+};
+
+
+int main()
+{
+    struct TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution s;
+    std::vector<int> result;
+    result = s.postorderTraversal(root);
+
+    for(auto i: result)
+    {
+        std::cout<<i<<'\t';
+    }
+    std::cout<<'\n';
+
+    return 0;
+}
